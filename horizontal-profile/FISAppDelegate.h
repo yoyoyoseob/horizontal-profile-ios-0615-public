582ac42d487a7fd297366fbae21e6f881023622a
Joe Burgess <joe@flatironschool.com>
2014-10-28 09:10:11 -0400

    Initial Commit

---
+++ horizontal-profile/FISAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  FISAppDelegate.h
+//  horizontal-profile
+//
+//  Created by iOS Staff on 10/28/14
+//  Copyright (c) 2014 The Flatiron School. All rights reserved.
+//
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
